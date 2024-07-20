ced4b7771fe72a3ac7ca21dd1f1019136da17817
Dmitriy Kasimov <its.dmitriy.kasimov@gmail.com>
2024-07-20 21:06:06 +0300

    tmp decision for the test work this service

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import * as alt from 'alt-client';
 import * as native from 'natives';
-import {WebView} from "alt-client";
+import {Player, showCursor, WebView} from "alt-client";
+import {freezeEntityPosition} from "natives";
 
 let browserCharacter: WebView = null;
-const playerid = alt.Player.local;
+const playerid = Player.local;
 
-alt.onServer("ServiceCharacter:s:ServiceCharacter:c:createCharacter", (characterJson: string) => {
+alt.onServer("s:c:createCharacter", (characterJson: string) => {
     browserCharacter = new WebView('http://resource/frontend/index.html');
-    browserCharacter.focus()
 
+    browserCharacter.focus()
+    showCursor(true)
+    freezeEntityPosition(playerid, true);
+
+    browserCharacter.on("f:c:createCharacter", (characterJson: string) => {
+        alt.log(characterJson);
+        alt.emitServer("c:s:createCharacter", characterJson);
+    })
 })
